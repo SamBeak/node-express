6c8e35c9009c2c0e1c1ca9fbd8bf52636e90eb95
Baek HyunGyu <baekhg102@gmail.com>
2023-07-27 17:48:26 +0900

    docs: Add new article

--- index.js
+++ index.js
@@ -9,9 +9,64 @@ const pool = mysql.createPool({
     database: "db_test",
   });
 
-//  pool.query(`CREATE DATABASE db_test`, function (err, results, fields) {
-//     console.log(results);
-//  });
- pool.query(`SHOW DATABASES`, function (err, results, fields) {
-    console.log(results);
- });
+
+    //  db_test database 생성
+    //  pool.query(`create database if not exists db_test`, function (err, results, fields) {
+    //     console.log(results);
+    //  });
+    
+    //  pool.query(`SHOW DATABASES`, function (err, results, fields) {
+    //     console.log(results);
+    //  });
+
+    // db talbe 생성
+    // pool.query(`create table notes(
+    //   uuid BINARY(16) DEFAULT (UUID_TO_BIN(UUID())),
+    //   title VARCHAR(255) NOT NULL,
+    //   contents TEXT NOT NULL,
+    //   created TIMESTAMP NOT NULL DEFAULT NOW()
+    // );`, function (err, results, fields) {
+    //   console.log(results);
+    // });
+
+    // db 내용 추가
+    // pool.query(`insert into notes(title, contents) values('My First Note', 'A note about something'), ('My Second Note', 'A note about something else');`, function (err, results, fields) {
+    //   console.log(results);
+    // });
+
+    // // db table 내용 조회
+    // pool.query(`select * from notes`, function (err, results, fields) {
+    //   console.log(results);
+    // });
+
+    // select 함수 만들기
+    function getNotes(){
+      // pool.query(`select * from notes`, function (err, results, fields) {
+      //   console.log(results);
+      // });
+      pool.query(`select BIN_TO_UUID(uuid,true) as uuid,title,contents,created from notes`, function (err, results, fields) {
+        console.log(results);
+      });
+    }
+
+    // UUID 를 버퍼에서 바이너리로 바꾸기
+    // pool.query(`select BIN_TO_UUID(uuid,true) as uuid,title,contents,created from notes`, function (err, results, fields) {
+    //   console.log(results);
+    // });
+
+    // insert 함수 만들기
+    function addNotes(title,contents){
+      pool.query(`insert into notes(title, contents) values('${title}','${contents}');`, function (err, results, fields) {
+        console.log(results);
+      });
+    };
+    // addNotes('Dataç','Dataç');
+
+    // update 함수 만들기
+    function updateNotes(uuid,title,contents){
+      pool.query(`update notes set title='${title}', contents='${contents}' where uuid=UUID_TO_BIN('${uuid}', 1);`, function (err, results, fields) {
+        console.log(results);
+      });
+    }
+    // updateNotes('2c5811ee-bd69-24ee-b729-0ac3b1522ed3','Data∂-updated','Data∂-updated');
+    getNotes();
